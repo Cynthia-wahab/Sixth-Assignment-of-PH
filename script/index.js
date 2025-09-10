a311de1dbb73311f34336da30147f114f7e137dc
Cynthia-wahab <cnthwahab@gmail.com>
2025-09-10 13:05:21 +0600

    added spinner

--- script/index.js
+++ script/index.js
@@ -2,12 +2,13 @@
 const categoriesContainer = document.getElementById('categoriesContainer')
 const plantsContainer = document.getElementById('plantsContainer')
 const allPlantsBtn = document.getElementById("all-trees")
-
+let cart = []
 
     
 
         
 const loadAllPlants = () => {
+    manageSpinner(true);
     fetch("https://openapi.programming-hero.com/api/plants")
     .then((res) => res.json())
     .then((data) => {
@@ -29,9 +30,7 @@ const loadAllPlants = () => {
                                     <span class="text-[12px] bg-green-200 text-green-800 px-2 py-1 rounded-lg">${plant.category}</span>
                                     <span class="font-semibold">৳${plant.price}</span>
                                 </div>
-                                <button class="bg-[#15803d] hover:bg-green-800 rounded-3xl text-white px-5 py-2 w-full">
-                                    Add to Cart
-                                </button>
+                                <button onclick='addToCart(${JSON.stringify(plant)})'class="bg-[#15803d] hover:bg-green-800 rounded-3xl text-white px-5 py-2 w-full"> Add to Cart</button>
                             </div>
                         `;
                 });
@@ -39,6 +38,7 @@ const loadAllPlants = () => {
     .catch((err) => {
         console.log(err);
     });
+    manageSpinner(false)
 };
 window.onload = () => {
     loadAllPlants();
@@ -47,6 +47,7 @@ window.onload = () => {
 
 
 const loadCategory = () =>{
+    manageSpinner(true)
     fetch("https://openapi.programming-hero.com/api/categories")
      .then((res) => res.json())
      .then((data) => {
@@ -56,9 +57,10 @@ const loadCategory = () =>{
         showCategories(categories)
     })
     .catch((err) => {
-        console.log(err);
-    });
-};
+        console.log(err)
+    })
+    manageSpinner(false)
+}
 
 const showCategories = (categories) => {
     categories.forEach(cat => {
@@ -110,10 +112,12 @@ const showPlantsByCategory = (plants) => {
                             <span class="  text-[12px] bg-green-200 text-green-800 px-2 py-1 rounded-lg">${plant.category}</span>
                             <span class="font-semibold">৳${plant.price}</span>
                         </div>
-                        <button class=" bg-[#15803d] rounded-3xl  text-white px-5 py-2 w-full my-5 ">Add to Cart</button>
+                    
+                        <button onclick='addToCart(${JSON.stringify(plant)})'class="bg-[#15803d] hover:bg-green-800 rounded-3xl text-white px-5 py-2 mt-3 w-full"> Add to Cart</button>
+
 
                     
-                    </div>
+        </div>
 
         `
 
@@ -121,15 +125,83 @@ const showPlantsByCategory = (plants) => {
     
 }
 
+// cart----------=>
+
+
+function addToCart(plant) {
+
+    let item = cart.find(p => p.id === plant.id)
+    if (item) {
+        item.quantity++;
+    } else {
+        cart.push({ ...plant, quantity: 1 })
+    }
+    showCart()
+}
+
+
+function showCart() {
+    const cartContainer = document.getElementById("cartContainer")
+    const cartTotal = document.getElementById("cartTotal")
+    cartContainer.innerHTML = ""
+    let total = 0;
+
+    cart.forEach(item => {
+        const itemTotal = item.price * item.quantity;
+        total += itemTotal
+
+    
+        const div = document.createElement("div");
+        div.className = "flex justify-between items-center  py-2"
+        div.innerHTML = `
+            <div>
+                <p class="font-semibold">${item.name}</p>
+                <p class="text-sm text-gray-500">৳${item.price} × ${item.quantity}</p>
+            </div>
+            <button class="text-red-500 remove-btn">❌</button>
+        `
+
+        div.querySelector(".remove-btn").addEventListener("click", () => {
+            removeFromCart(item.id)
+        })
+
+        cartContainer.appendChild(div)
+    });
+
+    cartTotal.textContent = "৳" + total
+}
+
+window.removeFromCart = function(id) {
+    cart = cart.filter(item => item.id !== id)
+    showCart();
+}
+
+
+
+
+
 const openPlantModal = (name, image, description, category, price) => {
-  document.getElementById("modalImage").src = image;
-  document.getElementById("modalTitle").innerText = name;
-  document.getElementById("modalDetails").innerText = description;
-  document.getElementById("modalCategory").innerText = category;
-  document.getElementById("modalPrice").innerText = `৳${price}`;
+  document.getElementById("modalImage").src = image
+  document.getElementById("modalTitle").innerText = name
+  document.getElementById("modalDetails").innerText = description
+  document.getElementById("modalCategory").innerText = category
+  document.getElementById("modalPrice").innerText = `৳${price}`
   
   my_modal_5.showModal();
 };
+
+
+const manageSpinner = (status) => {
+    if (status == true){
+        document.getElementById("spinner").classList.remove("hidden")
+        document.getElementById("plantsContainer").classList.add("hidden")
+    } else{
+        document.getElementById("plantsContainer").classList.remove("hidden")
+        document.getElementById("spinner").classList.add("hidden")
+
+    }
+}
+
 
 
 loadCategory()
